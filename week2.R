7879b6564ee9931bb277bb544f417fbf8e6553fb
HennessyAndrew <87822366+HennessyAndrew@users.noreply.github.com>
2021-12-28 11:28:57 -0500

    loaded data

--- week2.R
+++ week2.R
@@ -1,1 +1,10 @@
-#Starting week two cleanup data 
+#The dataset is activity.csv
+#The variables included in this dataset are:
+  # steps: Number of steps taking in a 5-minute interval (missing values are coded as \color{red}{\verb|NA|}NA)
+  # date: The date on which the measurement was taken in YYYY-MM-DD format
+  # interval: Identifier for the 5-minute interval in which measurement was taken
+  # The dataset is stored in a comma-separated-value (CSV) file and there are a total of 17,568 observations in this dataset.
+
+#Set directory setwd ("C:/Users/Charlotte/Documents/GitHub/ReproduceDataWeek2")
+#load data from directory
+DataFrame <- read.csv( "activity.csv")
